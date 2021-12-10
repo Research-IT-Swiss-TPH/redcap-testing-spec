cbc178b7d7f3246312cb482d321d967a4490e335
tertek <ekin.tertemiz@swisstph.ch>
2021-12-10 11:20:35 +0100

    Cleanup

--- cypress/integration/system_config.spec.js
+++ cypress/integration/system_config.spec.js
@@ -1,6 +1,8 @@
 // system_config.spec.js tests system configuration:
 // general settings
 // security settings
+// admin privileges
+// external modules
 
 
 //  <Spec Assertion Data>
@@ -8,32 +10,56 @@ const data_system_config = require('../../data/system_config.json')
 
 //  <paths>
 const path_redcap = '/redcap_v' + Cypress.env('version')
-const path_control_center = path_redcap + '/ControlCenter'
 
 describe('system config', () => {
-    
-    it('validates test user has admin priviliges', ()=>{
-        cy.visit( path_control_center + '/superusers.php')
-        cy.contains(Cypress.env('username'))
-    })
 
     it('validates general settings', () => {
-        cy.visit( path_control_center + '/general_settings.php')
+        cy.visit( path_redcap + '/ControlCenter/general_settings.php')
         
+        cy.get('select[name=language_global]').should("have.value", data_system_config.language_global)
         cy.get('input[name=redcap_base_url]').should("have.value", data_system_config.redcap_base_url)
         cy.get('input[name=redcap_survey_base_url]').should("have.value", data_system_config.redcap_survey_base_url)
+        cy.get('select[name=is_development_server]').should("have.value", data_system_config.is_development_server)
         cy.get('input[name=project_contact_email]').should("have.value", data_system_config.admin_email)
         
     })
 
     it('validates security settings', ()=>{
-        cy.visit( path_control_center + '/security_settings.php')
+        cy.visit( path_redcap + '/ControlCenter/security_settings.php')
 
         cy.get('select[name=two_factor_auth_enabled]').should("have.value", 1)
         cy.get('select[name=two_factor_auth_ip_check_enabled').should("have.value", 1)
         cy.get('textarea[name=two_factor_auth_ip_range').should("have.value", data_system_config.auth_ip_range)
         cy.get('input[name=two_factor_auth_ip_range_include_private]').should('be.checked')
     })
+
+    it('validates homepage settings', () => {
+        cy.visit( path_redcap + '/ControlCenter/homepage_settings.php')
+
+        cy.get('input[name=homepage_contact_email]').should('have.value', data_system_config.homepage_contact_email)
+    })    
+
+    it('validates project settings', () => {
+        cy.visit( path_redcap + '/ControlCenter/project_settings.php')
+
+        cy.get('select[name=project_language]').should('have.value', data_system_config.project_language)
+    })
+   
+    it('validates admin privileges', ()=>{
+        cy.visit( path_redcap + '/ControlCenter/superusers.php')
+        cy.contains(Cypress.env('username'))
+    })
+
+    it('validates external modules', () => {
+
+        cy.visit(path_redcap + '/ExternalModules/manager/control_center.php')
+
+        data_system_config.external_modules.forEach((em)=> {
+            cy.get('#external-modules-enabled')
+            .should('contain', em) 
+        })
+
+    })    
 
   })
 
--- cypress/integration/user_privileges.spec.js
+++
@@ -1,2 +0,0 @@
-// user_privileges.spec.js
-
